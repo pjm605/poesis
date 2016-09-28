093a4184bea8f8571e716457dad1ddf1aca14b02
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-09-28 16:36:49 -0400

    cleaned out frontend

--- browser/js/activity/activity.state.js
+++
@@ -1,9 +0,0 @@
-app.config(function ($stateProvider) {
-
-  $stateProvider.state('activity', {
-    url: '/activity/:location',
-    templateUrl: '/js/activity/activity.html',
-    controller: 'ActivityCtrl'
-  });
-
-});
