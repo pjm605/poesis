70a323d969a200cf40712f130c6cb2e05370c3d2
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-09-28 16:39:07 -0400

    finished cleaning out main controlelr

--- browser/js/main.controllers.js
+++ browser/js/main.controllers.js
@@ -1,18 +1,3 @@
-app.controller('MainCtrl', function($scope, MainFactory, $log, $state) {
-  
-  $scope.options = {
-    types: ['(cities)'],
-    componentRestrictions: { country: 'FR' }
-  };
-  
-  $scope.search = function (from, to) {
-    var cFrom = from.split(",")[0]
-    var cTo = to.split(",")[0]
-
-    $scope.cFrom = cFrom;
-    $scope.cTo = cTo;
-    $state.go('result', {from: cFrom, to: cTo})
-   
-  };
-  
+app.controller('MainCtrl', function($scope) {
+  // function that processes each word as it comes in
 });
