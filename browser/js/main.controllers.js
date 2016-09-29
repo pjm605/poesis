b92890f18a6a40011ca294be3ef0201541d4df87
Angela M. Choi <angela.minchoi@gmail.com>
2016-09-29 11:39:23 -0400

    added dictionaryFactory

--- browser/js/main.controllers.js
+++ browser/js/main.controllers.js
@@ -1,8 +1,19 @@
-app.controller('MainCtrl', function($scope) {
-  //console.log(input);
-  $scope.parent;
+app.controller('MainCtrl', function($scope, $log, DictionaryFactory, PhonemeFactory, SoundcountFactory) {
+  $scope.poem = {line: 0, word: ""};
   $scope.onSpace = function ($event) {
     console.log("on space event triggered");
-    console.log(input);
-  }
+    console.log($scope.poem.input);
+  };
+  $scope.onEnter = function ($event) {
+    console.log("onEnter event triggered");
+  };
+  $scope.updateWord = function () {
+    
+  };
+
+  DictionaryFactory.getDictionary()
+  .then(function (dictionary) {
+    $scope.dictionary = dictionary;
+  }).catch($log.error);
+  
 });
