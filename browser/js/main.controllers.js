03e01fe15a8f4c39c3994d5bacb6865109d6fe50
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-09-29 11:31:55 -0400

    trivial changes

--- browser/js/main.controllers.js
+++ browser/js/main.controllers.js
@@ -1,6 +1,7 @@
 app.controller('MainCtrl', function($scope) {
   //console.log(input);
   $scope.poem = {line: 0, word: ""};
+  
   $scope.onSpace = function ($event) {
     console.log("on space event triggered");
     console.log($scope.poem.input);
@@ -9,6 +10,6 @@ app.controller('MainCtrl', function($scope) {
     console.log("onEnter event triggered");
   }
   $scope.updateWord = function () {
-    
+
   }
 });
