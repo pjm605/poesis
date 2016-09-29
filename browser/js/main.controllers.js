db265762eba152884fb6f7bd2d572426d96c4552
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-09-29 11:13:14 -0400

    registers typed text on enter and on space

--- browser/js/main.controllers.js
+++ browser/js/main.controllers.js
@@ -1,11 +1,14 @@
 app.controller('MainCtrl', function($scope) {
   //console.log(input);
-  $scope.parent;
+  $scope.poem = {line: 0, word: ""};
   $scope.onSpace = function ($event) {
     console.log("on space event triggered");
-    //console.log(input);
+    console.log($scope.poem.input);
   }
   $scope.onEnter = function ($event) {
     console.log("onEnter event triggered");
+  }
+  $scope.updateWord = function () {
+    
   }
 });
