bc07999d165250679ecce684ff9948180da101fd
hannahlorane <hannah.katznelson@gmail.com>
2016-09-29 11:27:58 -0400

    Frontend (#8)
    * deleted cached node modules
    
    * registers typed text on enter and on space

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,7 +2,7 @@
 var router = require('express').Router(); // eslint-disable-line new-cap
 var dictionaryRouter = require('./dictionary');
 
-router.use('/dictionary', dictionaryRouter);
+// router.use('/dictionary', dictionaryRouter);
 
 
 // Make sure this is after all of
