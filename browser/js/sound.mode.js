c4c0070d7c3951549670b27dcc972c0a1815d244
hannahlorane <hannah.katznelson@gmail.com>
2016-09-30 17:05:37 -0400

    Modemirrorsetup (#24)
    * can pass in mode-specific configurations
    
    * new dataflow plan
    
    * crude sound annotation

--- browser/js/sound.mode.js
+++ browser/js/sound.mode.js
@@ -1,49 +1,18 @@
-CodeMirror.defineMode("lettermode", function () {
+CodeMirror.defineMode("lettermode", function (parserConfig) {
+  //local variables from parserConfig values go here!
+  var colorrules = parserConfig.mode.colorrules || [];
+  console.log(colorrules)
+  var colors = ['red', 'blue', 'green', 'yellow'];
   return {
     startState: function () {return {test: true};},
     token: function (stream, state) {
-      if (stream.next() == 'd') {
-        return 'error';
+      var n = stream.next();
+      for (var i = 0; i < colorrules.length; i++) {
+        if (colorrules[i].indexOf(n) > -1) {
+          return colors[i];
+        }
       }
-      else {
-        return null;
-      }
-      //return 'keyword';
+      return null;
     }
   }
 });
-
-
-/*
-
-CodeMirror.defineMode("lettermode", function () {
-  return {
-    startState: function () {return {between: false};},
-    token: function (stream, state) {
-
-
-      if (!state.between && stream.next() == '|') { //if we want to include | , else stream.peak == "|" and inside of {} state.next();
-        state.between = true;
-      
-        
-      }
-
-      if(state.between) {
-        if(stream.skipTo('|'))
-        {
-          stream.next();
-          state.between = false;
-        }
-        else {
-          stream.skipToEnd();
-        }
-        return "error"; 
-      }
-      else {
-        return null;
-      }
-      //return 'keyword';
-    }
-  }
-});
-*/
