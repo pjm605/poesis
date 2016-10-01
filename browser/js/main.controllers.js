cb6842125da83c46ae75eec244088fe3c452bc74
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-10-01 11:50:05 -0400

    cleaning linter

--- browser/js/main.controllers.js
+++ browser/js/main.controllers.js
@@ -6,7 +6,7 @@ app.controller('MainCtrl', function($scope, $document, $log, parse, soundsFactor
   //var textar = angular.element($document[0].querySelector('#poemarea'));
   var textar = document.getElementById('poemarea');
   //console.log(textar);
-  $scope.colorrules = soundToLetter('AH');
+  $scope.colorrules = soundToLetter('AH', 'D');
   console.log($scope.colorrules);
 
   var cm = CodeMirror.fromTextArea(textar, {
@@ -17,30 +17,30 @@ app.controller('MainCtrl', function($scope, $document, $log, parse, soundsFactor
     theme: "fontcolor"
   });
 
-  $scope.onSpace = function ($event) {
-    //this should parse the *whole* poem that has been written
-    //not just the most recent word
-    //and save it as an array of word parses
-    //which will be the argument for countSounds
-    console.log("on space event triggered");
-    var words = $scope.poem.input.split(' ');
-    $scope.poem.word = words[words.length-1];
-    var wordSounds = parse($scope.poem.word).then(function (p) {
-      console.log($scope.poem.word )
-      return p;
-    });
-    wordSounds.then(function (sounds) {
-      console.log(soundsFactory.countSounds(sounds));
-    });
-
-    $scope.lineEnd = false;
-  }
-  $scope.onEnter = function ($event) {
-    console.log("onEnter event triggered");
-    $scope.lineEnd = true;
-    // do anything line-dependent here?
-    $scope.onSpace();
-  }
+  // $scope.onSpace = function ($event) {
+  //   //this should parse the *whole* poem that has been written
+  //   //not just the most recent word
+  //   //and save it as an array of word parses
+  //   //which will be the argument for countSounds
+  //   console.log("on space event triggered");
+  //   var words = $scope.poem.input.split(' ');
+  //   $scope.poem.word = words[words.length-1];
+  //   var wordSounds = parse($scope.poem.word).then(function (p) {
+  //     console.log($scope.poem.word )
+  //     return p;
+  //   });
+  //   wordSounds.then(function (sounds) {
+  //     console.log(soundsFactory.countSounds(sounds));
+  //   });
+  //
+  //   $scope.lineEnd = false;
+  // }
+  // $scope.onEnter = function ($event) {
+  //   console.log("onEnter event triggered");
+  //   $scope.lineEnd = true;
+  //   // do anything line-dependent here?
+  //   $scope.onSpace();
+  // }
 
   //every time the user types a letter, it adds to $scope.text.
   // console.log $scope.text when the user stops typing for more than 2 seconds.
@@ -48,7 +48,7 @@ app.controller('MainCtrl', function($scope, $document, $log, parse, soundsFactor
 
   var debounced = _.debounce(function(cm, obj) {
     console.log('obj.text', $scope.text);
-    //could pass in the updated $scope.text to a function here after the user stops typing. 
+    //could pass in the updated $scope.text to a function here after the user stops typing.
   }, 2000);
 
   var beforeDebounced = function(cm, obj){
--- browser/js/soundtoletter.factory.js
+++ browser/js/soundtoletter.factory.js
@@ -1,33 +1,36 @@
 app.factory('soundToLetter', function() {
-  return function (sound) {
-    sound = sound.toLowerCase();
+  return function () {
+    var sounds = [].prototype.slice.call(this.arguments);
   	var result = [];
   	var rule = {
-	"iy" : "ea",
-	"ih" : "i",
-	"eh" : "e",
-	"ae": "a",
-	"aa" : "o",
-	"ax" : "e",
-	"ah": "u",
-	"uw" : "oo",
-	"uh": "oo",
-	"aw" : "ou",
-	"er" : "ir",
-	"axr": "er",
-	"ey" : "ai",
-	"ay": "i",
-	"oy" : "o",
-	"ow": "oa",
-	"ao" : "ou",
-	"zh": "s",
-	"dx": "tt",
-	"el" : "le",
-	"em" : "om",
-	"en" : "n"
-};
-	 if(rule.hasOwnProperty(sound)) result.push(rule[sound]);
-	 else result.push(sound);
-	 return result;
+      iy: 'ea',
+	    ih: 'i',
+    	eh: "e",
+	    ae: "a",
+      aa: "o",
+      ax: "e",
+      ah: "u",
+      uw: "oo",
+      uh: "oo",
+      aw: "ou",
+      er: "ir",
+      axr: "er",
+      ey: "ai",
+      ay: "i",
+      oy: "o",
+      ow: "oa",
+      ao: "ou",
+      zh: "s",
+      dx: "tt",
+      el: "le",
+      em: "om",
+      en: "n"
+    };
+    for (var i = 0; i < sound; i++) {
+      sound = sounds[i].toLowerCase();
+      if(rule.hasOwnProperty(sound)) result.push(rule[sound]);
+   	  else result.push(sound);
+    }
+	  return result;
  };
 });
