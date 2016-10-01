1e7dd4fd8db50f7627e891d98cce62980b4a19b5
Angela M. Choi <angela.minchoi@gmail.com>
2016-09-30 21:57:35 -0400

    last commit before git pull

--- browser/js/main.controllers.js
+++ browser/js/main.controllers.js
@@ -1,48 +1,35 @@
-app.controller('MainCtrl', function($scope, $document, $log, parse, soundsFactory, $interval) {
+app.controller('MainCtrl', function($scope, $document, $log, parse, soundsFactory) {
+  //$scope, $document, $log, parse, soundsFactory, soundToLetter
   $scope.poem = {line: 0, word: ""};
   $scope.lineEnd = false;
-  $scope.textArea = null;
 
   // console.log(CodeMirror);
   //var textar = angular.element($document[0].querySelector('#poemarea'));
   var textar = document.getElementById('poemarea');
-  // console.log(textar);
+  //console.log(textar);
+  $scope.colorrules = soundToLetter('AH');
+  // console.log($scope.colorrules);
+
   var cm = CodeMirror.fromTextArea(textar, {
-    mode: "lettermode",
+    mode: {
+      name: "lettermode",
+      colorrules: $scope.colorrules
+    },
     theme: "fontcolor"
   });
 
-  // function showCode() {
-  //   $scope.textArea = cm.getValue();
-  //   // document.getElementById('poemarea').val($scope.textArea);
-  //   var poemarea = angular.element(document.querySelector('#poemarea'));
-  //   poemarea.text($scope.textArea);
-  // }
-  // $interval(showCode, 3000);
-  $scope.text = "";
-
-  var debounced = _.debounce(function(cm, obj) {
-    console.log('obj.text', $scope.text);
-  }, 2000);
-
-  var beforeDebounced = function(cm, obj){
-    $scope.text += obj.text;
-    debounced();
-  }
-
-  cm.on('change', beforeDebounced);
-
-
   $scope.onSpace = function ($event) {
+    //this should parse the *whole* poem that has been written
+    //not just the most recent word
+    //and save it as an array of word parses
+    //which will be the argument for countSounds
     console.log("on space event triggered");
-
     var words = $scope.poem.input.split(' ');
     $scope.poem.word = words[words.length-1];
     var wordSounds = parse($scope.poem.word).then(function (p) {
       console.log($scope.poem.word )
       return p;
     })
-
     wordSounds.then(function (sounds) {
       console.log(soundsFactory.countSounds(sounds));
     })
@@ -55,5 +42,19 @@ app.controller('MainCtrl', function($scope, $document, $log, parse, soundsFactor
     // do anything line-dependent here?
     $scope.onSpace();
   }
- 
+
+  //every time the user types a letter, it adds to $scope.text.
+  // console.log $scope.text when the user stops typing for more than 2 seconds.
+  $scope.text = "";
+
+  var debounced = _.debounce(function(cm, obj) {
+    console.log('obj.text', $scope.text);
+  }, 2000);
+
+  var beforeDebounced = function(cm, obj){
+    $scope.text += obj.text;
+    debounced();
+  }
+
+  cm.on('change', beforeDebounced);
 });
