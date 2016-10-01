a3366151f7cae5ee1568284738faeefc15e23e99
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-10-01 12:41:20 -0400

    countSounds now handles whole (preprocessed) text instead of just a single word)

--- browser/js/sound.factory.js
+++ browser/js/sound.factory.js
@@ -1,35 +1,44 @@
 app.factory('soundsFactory', function () {
-  var soundDic = {};
-  return {
-      countWordSounds: function (word) {
-        var newWord = word.replace(/[^A-Za-z ]+/g, '');
-        newWord = newWord.split(" ");
-        for (var i = 0; i < newWord.length; i++) {
-          if(soundDic.hasOwnProperty(newWord[i]) === false) soundDic[newWord[i]] = 1;
-          else soundDic[newWord[i]] += 1;
-        }
-      },
-      getSoundDic: function () {
-        return soundDic;
-      },
-      identifySignificant: function (sounds) {
-        /*
-          takes in an object containing the counts of all the sounds throughout
-          the poem, and returns an array of significant sounds
-        */
-        // input: { AH: 3, JH: 1, UW: 1, D: 1, K: 1, EY: 1, SH: 1, N: 1 }
-        // output: ["AH"]
-        var significantN = 0
-        var significant = ""
-        for (var key in sounds) {
-          if(sounds[key] > significantN) {
-            significantN = sounds[key]
-            significant = key
-          }
-        }
-        return [significant];
+
+  //helper function we do not want to expose
+  var countTextSounds = function (text) {
+    var soundDic = {};
+    var countWordSounds = function (word) {
+      var newWord = word.replace(/[^A-Za-z ]+/g, '');
+      newWord = newWord.split(' ');
+      for (var i = 0; i < newWord.length; i++) {
+        if(soundDic.hasOwnProperty(newWord[i]) === false) soundDic[newWord[i]] = 1;
+        else soundDic[newWord[i]] += 1;
       }
     };
+    for (var i = 0; i < text.length; i++) {
+      countWordSounds(words[i]);
+    }
+    return soundDic
+  };
+
+  return {
+    //the text which is input should be an array of arrays of
+    //phonetically parsed words
+    identifySignificant: function (text) {
+      return countTextSounds(text);
+      //temporarily commented out for the purpose of testing
+      /*
+      var sounds = countTextSounds(text);
+      // input: { AH: 3, JH: 1, UW: 1, D: 1, K: 1, EY: 1, SH: 1, N: 1 }
+      // output: ["AH"]
+      var significantN = 0
+      var significant = ""
+      for (var key in sounds) {
+        if(sounds[key] > significantN) {
+          significantN = sounds[key]
+          significant = key
+        }
+      }
+      return [significant];
+      */
+    }
+  };
 });
 
 //
