6952e5560deddbb4366052fa78c006c4995f8dae
Angela M. Choi <angela.minchoi@gmail.com>
2016-10-05 16:34:48 -0400

    saved for now

---
+++ browser/js/factories/parse.factory.js
@@ -0,0 +1,12 @@
+app.factory('parse', function($http) {
+    var dictionary = $http.get('/api/lexicon/cmudictionary')
+        .then(function(response) {
+            return response.data;
+        });
+    return function(word) {
+        return dictionary.then(function(dict) {
+            if (dict[word]) return dict[word];
+            else return 'hapax';
+        })
+    }
+});
--- browser/js/main.controllers.js
+++ browser/js/main.controllers.js
@@ -1,4 +1,4 @@
-app.controller('MainCtrl', function($scope, $document, $log, soundFactory, lexicon) {
+app.controller('MainCtrl', function($scope, $document, $log, soundFactory, lexicon, parse) {
   $scope.poem = {line: 0, word: ''};
   $scope.lineEnd = false;
 
@@ -20,11 +20,38 @@ app.controller('MainCtrl', function($scope, $document, $log, soundFactory, lexic
     //$scope.text gets updated when the user stops typing for more than 2 seconds.
     console.log('$scope.text', $scope.text);
     //could pass in the updated $scope.text to a function here
-    var words = $scope.text.replace(/ /g, '\n');
-    
-    lexicon(words)
-    .then(function(parseArray) {
-      console.log('parseArray', parseArray);
+
+    var words = $scope.text.replace(/-/g, ' ').split(' ');
+    // var words = $scope.text.replace(/ /g, '\n');
+    var parsedWords = [];
+    var hapaxWords = [];
+    var fromLexicon;
+    for (var w = 0; w < words.length; w++) {
+      parsedWords.push(parse(words[w]));
+    }
+    console.log('parsedWords', parsedWords);
+
+    Promise.all(parsedWords).then(function (parseArray) {
+      for(var i = 0; i < parseArray.length; i ++) {
+        if (parseArray[i] === 'hapax') {
+          hapaxWords.push(parseArray[i]);
+        }
+      }
+      hapaxWords = hapaxWords.join('\n');
+      console.log('hapaxWords', hapaxWords);
+
+      lexicon(hapaxWords)
+      .then(function(lexiconParseArray) {
+        fromLexicon = lexiconParseArray;
+        console.log('fromLexicon', fromLexicon);
+      });
+
+      for(var i = 0; i < parsedWords.length; i ++) {
+        if (parsedWords[i] === fromLexicon[0]) {
+          parsedWords[i] = fromLexicon.shift();
+        }
+      }
+      
       soundFactory.main(parseArray, cm);
     });
 
