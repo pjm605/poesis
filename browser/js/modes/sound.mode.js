52f7db7587a9f5cab09fb2ec1c1f78b3444c06f9
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-10-05 11:39:59 -0400

    vowels basically plugged in; some bugs

--- browser/js/modes/sound.mode.js
+++ browser/js/modes/sound.mode.js
@@ -5,30 +5,44 @@ CodeMirror.defineMode('soundMode', function (config, parserConfig) {
   var simpleVowels = ['a', 'e', 'i', 'o', 'u'];
   var consonantRules = parserConfig.consonantRules;
   var vowelLocations = parserConfig.vowelLocations;
-  var colors = ['red', 'blue', 'green', 'purple'];
+  var colors = ['red', 'green', 'purple', 'yellow'];
+  var currentPositions = {};
+  for (var vow in vowelLocations) {
+    currentPositions[vow] = 0;
+  }
+  console.log('vowel locations -->', vowelLocations);
 
   return {
     startState: function() {return {
-      counting: [0, -1],
-      token: this.token
+      position: [0, -1],
+      //token: this.token
     };},
     token: function (stream, state) {
       var next = stream.next();
-      if (next == ' ') {
+      if (next == ' ' || !next) {
         //next token is a space
-        state.counting[0]++;
-        state.counting[1] = -1;
+        state.position[0]++;
+        state.position[1] = -1;
         return null;
       }
       else {
         //next token is a vowel
         if (simpleVowels.indexOf(next) > -1 ||
         (next == 'y' && simpleVowels.indexOf(stream.peek()) < 0)) {
+          console.log('inithe vowel branch!!!');
           while (simpleVowels.indexOf(stream.peek()) > -1 || stream.peek() == 'y') {
             stream.next();
           }
-          state.counting[1]++;
-          return 'blue';
+          state.position[1]++;
+          for (var vow in vowelLocations) {
+            var nextVowel = vowelLocations[vow][currentPositions[vow]];
+            console.log(state.position, "state.position");
+            console.log(nextVowel, 'nextvowel');
+            if (state.position[0] == nextVowel[0] && state.position[1] == nextVowel[1]) {
+              currentPositions[vow]++;
+              return 'blue';
+            }
+          }
         }
         else {
           //next token is a consonant
@@ -38,7 +52,6 @@ CodeMirror.defineMode('soundMode', function (config, parserConfig) {
               for (var char = 1; char < consonantRules[i].length; char++) {
                 if (consonantRules[i][char] != stream.next()) {
                   stream.backUp(char);
-                  break;
                 }
                 else return colors[i];
               }
