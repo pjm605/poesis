a1614552991a5f2847ae5b024739a243f4ff2798
pjm605 <pjm605@gmail.com>
2016-10-06 17:28:52 -0400

    Rhyme (#57)
    * sounds.factory identifySignificant function
    
    * working on angular testing
    
    * browser testing setup
    
    * basic testing setup done
    
    * correct capitalization
    
    * fixed misspelling
    
    * working factory test
    
    * server indexContent testing done
    
    * middle of rhyme factory
    
    * rhyme
    
    * merge with meter
    
    * find word location in text and vowel location in word
    
    * rhyme mode
    
    * rhyme factory done

---
+++ browser/js/modes/rhyme.mode.js
@@ -0,0 +1,8 @@
+// CodeMirror.defineMode('rhymeMode', function (config, parserConfig)) {
+// 	return {
+// 		startState: function () {return {};},
+// 		token: function (stream, state) {
+
+// 		};
+// 	};
+// });
