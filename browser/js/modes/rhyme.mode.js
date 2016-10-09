37a41c3410f9e071fc9fcc05fd6fca0b494ff633
Angela M. Choi <angela.minchoi@gmail.com>
2016-10-09 14:20:09 -0400

    marks til the end of the last word

--- browser/js/modes/rhyme.mode.js
+++ browser/js/modes/rhyme.mode.js
@@ -2,6 +2,7 @@ CodeMirror.defineMode('rhymeMode', function (config, parserConfig) {
 var simpleVowels = ['a', 'e', 'i', 'o', 'u'];
 var rhymeLocations = parserConfig.rhymeLocations;
 var rhymeColors = ['blue', 'red', 'green', 'orange', 'yellow'];
+var locations = [];
 
 	return {
 		startState: function () {
@@ -20,26 +21,21 @@ var rhymeColors = ['blue', 'red', 'green', 'orange', 'yellow'];
 					state.counting[1]++;
 					for (var i = 0; i < rhymeLocations.length; i++) {
 						for (var j = 0; j < rhymeLocations[i].length; j++) {
-							var locations = [];
 							locations[0] = rhymeLocations[i][j][0];
 							locations[1] = rhymeLocations[i][j][1];
-							console.log('locations', locations);
-							console.log('state.counting***', state.counting);
 							if (locations[0] === state.counting[0] && locations[1] === state.counting[1]){
 								console.log('MATCHING', [state.counting[0], state.counting[1]]);
-								console.log(stream.peek());
-								// while (stream.peek() !== ' ') {
-								// 	stream.next();
-								// // 	// return rhymeColors[i];
-								// }
+								if (stream.peek() !== ' ') {
+									stream.skipToEnd();
+								}
 								state.counting[0]++;
 								state.counting[1] = -1;
 								return rhymeColors[i];
 							}
 						}
 					}
-					return null;
 				}
+				return null;
 			}
 		}
 	}
