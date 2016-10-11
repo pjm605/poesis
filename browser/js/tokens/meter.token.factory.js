d44c7a01add7b2b1be7ce710d13f80b30a88225f
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-10-11 15:05:45 -0400

    fixed hanging word meter bug

--- browser/js/tokens/meter.token.factory.js
+++ browser/js/tokens/meter.token.factory.js
@@ -1,18 +1,22 @@
 app.factory('meterToken', function () {
   //state.position = [word#, v#]
+  //the hanging word bug seems to be caused by
+  //state.position[0] failing to increment
   return function (stream, state, findToken, parserConfig, isVowel, currentPositions) {
     var next = findToken(stream);
-    if (!next) {
+    if (!next || stream.eol()) {
       state.position[0]++;
       state.position[1] = -1;
     }
     if (isVowel(next, stream) && parserConfig.stresses.length) {
       state.position[1]++;
+      console.log('NEXT I AM DEALING WITH THIS VOWEL NOW', next);
       console.log('STRESSES', parserConfig.stresses)
-      console.log(state.position, '<--- state.position, brosphine');
-      //var str = stresses[state.position[0]][state.position[1]];
+      console.log(state.position[0], '<--- state.position, brosphine');
+      console.log('this hopefully does exist -->', parserConfig.stresses)
+      var str = parserConfig.stresses[state.position[0]][state.position[1]];
     //  console.log('current stress: ', str);
-      switch ('a') {
+      switch (str) {
         case 'a':
           return 'turquoise';
         case 's':
