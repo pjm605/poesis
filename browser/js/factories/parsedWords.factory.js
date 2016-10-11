1b8fa1e0bde3a200d8f820325a9dc3d7a41d1e59
Angela Choi <angela.minchoi@gmail.com>
2016-10-11 11:58:14 -0400

    refactored main controller (#69)
    * isVowel
    
    * refactored main.controller
    
    * parseWords factory refactored

---
+++ browser/js/factories/parsedWords.factory.js
@@ -0,0 +1,39 @@
+app.factory('parseWordsFactory', function (meterToken, meterFactory, rhymeToken, soundToken, lexicon, parse) {
+
+  var parsedWords = [];
+  var hapaxWords = [];
+  var fromLexicon = null;
+  var lineArray;
+
+  return function(words) {
+    for (var idx = 0; idx < words.length; idx++) {
+      parsedWords.push(parse(words[idx]));
+    }
+
+    return Promise.all(parsedWords).then(function (parseArray) {
+      for (var i = 0; i < parseArray.length; i++) {
+        if (parseArray[i][0] === '@') {
+          hapaxWords.push(parseArray[i].slice(1));
+        }
+      }
+      console.log('Not in the dictionary: hapaxWords', hapaxWords);
+      if (hapaxWords.length > 0) {
+        hapaxWords = hapaxWords.join('\n');
+        return lexicon(hapaxWords)
+        .then(function(lexiconParseArray) {
+          fromLexicon = lexiconParseArray;
+          for (var j = 0; j < parseArray.length; j++) {
+            if (parseArray[j][0] === '@') {
+              parseArray[j] = fromLexicon.shift();
+            }
+          }
+        });
+      }
+      return parseArray;
+    })
+    .catch(function (err) {
+      console.error('error', err);
+    });
+
+  }
+});
