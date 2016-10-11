c048350aa83694308ff5d32e956385dba914f225
Janice Park <pjm605@gmail.com>
2016-10-11 15:11:55 -0400

    before merge with toggling

--- browser/js/module.js
+++ browser/js/module.js
@@ -26,6 +26,6 @@ var app = angular.module('myApp', ['ui.router', 'ngMessages']);
 // );
 
 
-app.config(function ($locationProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/poesis');
-});
+// app.config(function ($locationProvider, $urlRouterProvider) {
+//   $urlRouterProvider.otherwise('/poesis');
+// });
