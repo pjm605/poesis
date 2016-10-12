22691a0913cde327e9696ef2e35b4c30a633f13f
pjm605 <pjm605@gmail.com>
2016-10-12 12:38:52 -0400

    Test (#83)
    * lexicon factory test done
    
    * before merge

---
+++ tests/browser/factories/lexiconFactory-test.js
@@ -0,0 +1,45 @@
+describe('lexiconFactory', function () {
+	beforeEach(module('myApp'));
+
+	var lexiconFactory;
+	beforeEach('Get tools', inject(function (_$httpBackend_, _lexiconFactory_) {
+		$httpBackend = _$httpBackend_;
+		lexiconFactory = _lexiconFactory_;
+	}));
+
+	describe('fromLexiconServer', function () {
+		var data;
+		var responseData;
+
+		beforeEach(function () {
+			data =  "aabbcc"
+			responseData = ["AA B K"];
+
+			$httpBackend
+				.expectPOST('/api/lexicon', { "words": data })
+				.respond(responseData);
+		});
+
+		afterEach(function () {
+			 $httpBackend.verifyNoOutstandingExpectation();
+		});
+
+		it('makes the expected POST request with expected data', function () {
+            lexiconFactory.fromLexiconServer(data);
+            $httpBackend.flush();
+        });
+
+        it('returns a promise that resolves to the data of the response', function (done) {
+            lexiconFactory.fromLexiconServer(data).then(function (dict) {
+            	expect(dict).to.be.deep.equal(responseData);
+            })
+            .catch(done)
+            $httpBackend.flush();      
+            done();       
+        });
+
+
+	})
+	
+});
+
---
+++ tests/browser/factories/parseFactory-test.js
@@ -0,0 +1,27 @@
+// describe('parseFactory', function () {
+// 	beforeEach(module('myApp'));
+
+// 	var parseFactory;
+// 	beforeEach('Get tools', inject(function (_$httpBackend_, _parseFactory_) {
+// 		$httpBackend = _$httpBackend_;
+// 		parseFactory = _parseFactory_;
+// 	}));
+
+// 	describe('fromDictionary', function () {
+// 		var word;
+// 		var response;
+
+// 		beforeEach(function () {
+// 			word =  "addddbc"
+// 			response = '@addddbc';
+// 		});
+
+// 		it('returns a promise that resolves to the data of the response', function () {
+//   			parseFactory.fromDictionary(word)
+//   			.then(function (message) {
+//   				expect(message).to.be.deep.equal(response);
+//   			})
+//         });
+// 	})
+	
+// });
