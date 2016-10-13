80c3833dc1c1ee4ce05c6555f1a8f7a3ce7accdd
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-10-13 11:51:33 -0400

    fixed gh bug

--- browser/js/main.mode.js
+++ browser/js/main.mode.js
@@ -10,7 +10,7 @@ CodeMirror.defineMode('mainMode', function (config, parserConfig) {
     i++;
   }
 
-  var clusters = ['tio', 'sh', 'th', 'ng', 'ch', 'ie', 'ou', 'ei', 'qu', 'ey', 'oy', 'ay', 'au', 'uy', 'oi', 'ee', 'ai', 'ow', 'ea', 'oo'];
+  var clusters = ['tio', 'sh', 'th', 'ng', 'ch', 'gh', 'ie', 'ou', 'ei', 'qu', 'ey', 'oy', 'ay', 'au', 'uy', 'oi', 'ee', 'ai', 'ow', 'ea', 'oo'];
 
 
   var isVowel = function (tok, stream) {
