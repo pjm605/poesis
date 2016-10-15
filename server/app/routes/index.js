0ac9891c56e380b83ce03b111f4a735f96d55d9a
Angela M. Choi <angela.minchoi@gmail.com>
2016-10-14 23:56:26 -0400

    favicon, linter errors, indentation

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,12 +2,12 @@
 var router = require('express').Router(); // eslint-disable-line new-cap
 var lexiconRouter = require('./lexicon');
 
- router.use('/lexicon', lexiconRouter);
-
+router.use('/lexicon', lexiconRouter);
 
 // Make sure this is after all of
 // the registered routes!
 router.use(function (req, res) {
-    res.status(404).end();
+  res.status(404).end();
 });
+
 module.exports = router;
