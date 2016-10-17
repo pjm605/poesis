836e27a1d266507addb2bb1c8a13fcda8fa4f0e6
hannahlorane <hannah.katznelson@gmail.com>
2016-10-17 11:21:03 -0400

    Bugs (#105)
    * stresses array in mode is empty
    
    * trivial bug fix

--- browser/js/main.mode.js
+++ browser/js/main.mode.js
@@ -10,7 +10,7 @@ CodeMirror.defineMode('mainMode', function (config, parserConfig) {
     i++;
   }
 
-  var clusters = ['tio', 'sh', 'wh', 'th', 'ng', 'ch', 'gh', 'ie', 'ou', 'ei', 'qu', 'ey', 'oy', 'ay', 'au', 'uy', 'oi', 'ee', 'ai', 'ow', 'ea', 'oo'];
+  var clusters = ['tio', 'sh', 'wh', 'th', 'ng', 'ch', 'gh', 'ie', 'le', 'ou', 'ei', 'll', 'qu', 'ey', 'oy', 'ay', 'au', 'uy', 'oi', 'ee', 'ai', 'ow', 'ea', 'oo'];
 
   var isVowel = function (tok, stream) {
     // does not modify the stream
